d0820af5683ca9c6c5edb7687cf2728fd3b87664
beavercat91 <71858055+beavercat91@users.noreply.github.com>
2020-09-29 14:25:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ $(document).ready(function () {
 		$('.menu-toggler').removeClass('open');
 		$('.top-nav').removeClass('open');
 	});
-
+	
 	$('nav a[href*="#"]').on('click', function () {
-		$('html, body').animate( keyframes: {
+		$('html, body').animate( {
 			scrollTop: $($(this).attr('href')).offset().top - 100
-		}, options: 2000);
+		});
 	});
 
 	$('#up').on('click', function () {
-		$('html, body').animate( keyframes: {
+		$('html, body').animate(  {
 			scrollTop: 0
-		}, options: 2000);
+		});
 	});
 
 	AOS.init({
