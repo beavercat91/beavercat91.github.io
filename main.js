1756e1e1127ab6033eefa1a185a5fba738ffc72c
beavercat91 <71858055+beavercat91@users.noreply.github.com>
2020-09-29 18:14:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,20 @@ $(document).ready(function () {
 		});
 	});
 
+	const nav = $("#navigationBar");
+  const navTop = nav.offset().top;
+  $(window).on("scroll", stickyNavigation);
+  function stickyNavigation() {
+      var body = $("body");
+      if($(window).scrollTop() >= navTop) {
+          body.css("padding-top", nav.outerHeight() + "px");
+          body.addClass("fixedNav");
+      } else {
+          body.css("padding-top", 0);
+          body.removeClass("fixedNav");
+		  }
+		 }
+
 	AOS.init({
 		easing: 'ease',
 		duration: 1800
