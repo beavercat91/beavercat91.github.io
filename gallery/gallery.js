8fce057c0606a2a6a785ee339ef6663acfa2dd60
beavercat91 <71858055+beavercat91@users.noreply.github.com>
2020-10-10 15:09:41 -0500

    Update gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,15 +1,3 @@
-HTML SCSS JSResult
-EDIT ON
-const swiper = new Swiper('.swiper-container', {
-  direction: 'vertical',
-  mousewheel: {},
-  effect: 'cube',
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false
-  }
-});
-
 $(document).ready(function(){
     $("#up").click(function(event){
         event.preventDefault();
